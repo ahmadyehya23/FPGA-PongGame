50fe59c8fdf324ae5abfe585eb4a80929c9e2d23
ahmadyehya23 <ahmadyehya23@gmail.com>
2026-07-22 21:52:12 -0700

    Connected FPGA modules in top level

--- rtl/top.sv
+++ rtl/top.sv
@@ -1,25 +1,52 @@
 module top (
 
-    input  logic CLOCK_50,
-    input  logic [1:0] KEY,
+    input logic CLOCK_50,
+    input logic [1:0] KEY,
 
     output logic VGA_HS,
     output logic VGA_VS,
+
     output logic [3:0] VGA_R,
     output logic [3:0] VGA_G,
     output logic [3:0] VGA_B
 
 );
 
+
+    // =========================
+    // Internal signals
+    // =========================
+
+    logic reset;
+
+    logic game_enable;
+
     logic [9:0] pixel_x;
     logic [9:0] pixel_y;
+
     logic video_on;
 
-    // VGA timing generator
+
+    logic [9:0] ball_x;
+    logic [9:0] ball_y;
+
+    logic [9:0] paddle_left_y;
+    logic [9:0] paddle_right_y;
+
+
+
+    assign reset = ~KEY[0];
+
+
+
+    // =========================
+    // VGA Controller
+    // =========================
+
     vga_controller vga_inst (
 
         .clk(CLOCK_50),
-        .reset(~KEY[0]),
+        .reset(reset),
 
         .hsync(VGA_HS),
         .vsync(VGA_VS),
@@ -31,9 +58,63 @@ module top (
 
     );
 
-    // Temporary display
-    assign VGA_R = video_on ? 4'hF : 4'h0;
-    assign VGA_G = 4'h0;
-    assign VGA_B = 4'h0; 
+
+
+    // =========================
+    // Game clock enable
+    // =========================
+
+    clock_enable clock_inst (
+
+        .clk(CLOCK_50),
+        .reset(reset),
+
+        .enable(game_enable)
+
+    );
+
+
+
+    // =========================
+    // Pong Engine
+    // =========================
+
+    pong_engine game_inst (
+
+        .clk(CLOCK_50),
+        .reset(reset),
+        .enable(game_enable),
+
+        .ball_x(ball_x),
+        .ball_y(ball_y),
+
+        .paddle_left_y(paddle_left_y),
+        .paddle_right_y(paddle_right_y)
+
+    );
+
+
+
+    // =========================
+    // Renderer
+    // =========================
+
+    renderer render_inst (
+
+        .pixel_x(pixel_x),
+        .pixel_y(pixel_y),
+
+        .ball_x(ball_x),
+        .ball_y(ball_y),
+
+        .paddle_left_y(paddle_left_y),
+        .paddle_right_y(paddle_right_y),
+
+        .red(VGA_R),
+        .green(VGA_G),
+        .blue(VGA_B)
+
+    );
+
 
 endmodule
